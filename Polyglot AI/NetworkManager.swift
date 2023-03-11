f799d31acb3da413b5e817afc68d41f57ab27aef
Mayowa Paul <lexypaul13@yahoo.com>
2023-03-11 17:10:02 -0500

    Added Loading animation Added post request to open AI API

--- Polyglot AI/NetworkManager.swift
+++ Polyglot AI/NetworkManager.swift
@@ -6,47 +6,77 @@
 //
 
 import Foundation
-import OpenAISwift
+enum HTTPError: Error {
+    case invalidURL
+    case invalidResponse(Data?, URLResponse?)
+}
 
-
-class NetworkManager{
+class NetworkManager {
     static let shared = NetworkManager()
-    
-    @frozen enum Constants{
-        static let key = "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"
-    }
-    
-    
-    private var client: OpenAISwift?
-    
+    private let baseURL = "https://api.openai.com/v1/completions"
+    private let apiHeaders = [
+        "Content-Type": "application/json",
+        "Authorization": "Bearer "
+    ]
     private init() {}
-    
-    func setUp(){
-        self.client = OpenAISwift(authToken: Constants.key)
-    }
-    func getResponse(input: String, completion: @escaping (Result<String, Error>) -> Void) {
-        let prompt = """
-            translate from English to Nigerian Pidgin:
 
-            \(input)
-
-            Output:
-            """
-
+    func getTranslation(input: String, completion: @escaping (Result<String, Error>) -> Void) {
+        let parameters: [String: Any] = [
+            "model": "text-davinci-003",
+            "prompt": "Translate this into pidgin english nigeria \n\n\(input)\n",
+            "temperature": 0.3,
+            "max_tokens": 100,
+            "top_p": 1,
+            "frequency_penalty": 0,
+            "presence_penalty": 0
+        ]
         
-        client?.sendCompletion(with: prompt, model: .gpt3(.davinci), maxTokens: 60) { result in
-            switch result {
-            case .success(let response):
-                let output = response.choices.first?.text ?? ""
-                print(response)
-                completion(.success(output))
-            case .failure(let error):
+        let url = URL(string: baseURL)!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = apiHeaders
+        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
                 completion(.failure(error))
-                print(error.localizedDescription)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                httpResponse.statusCode == 200,
+                let responseData = data else {
+                completion(.failure(HTTPError.invalidResponse(data, response)))
+                return
+            }
+            
+            do {
+                let message = try self.parseTextCompletionResponse(responseData)
+                completion(.success(message))
+            } catch {
+                completion(.failure(error))
             }
         }
+        
+        task.resume()
     }
     
-    
-    
+    private func parseTextCompletionResponse(_ data: Data) throws -> String {
+        print("Text completion response data: \(String(data: data, encoding: .utf8) ?? "")")
+        
+        let jsonObject = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+        guard let choices = jsonObject?["choices"] as? [[String: Any]] else {
+            throw HTTPError.invalidResponse(data, nil)
+        }
+        
+        if choices.isEmpty {
+            throw HTTPError.invalidResponse(data, nil)
+        }
+        
+        guard let text = choices[0]["text"] as? String else {
+            throw HTTPError.invalidResponse(data, nil)
+        }
+        
+        return text.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
 }
--- Polyglot AI/ViewController.swift
+++ Polyglot AI/ViewController.swift
@@ -6,44 +6,68 @@
 //
 
 import UIKit
-
+import Lottie
 class ViewController: UIViewController {
     
     @IBOutlet weak var firstTextField: UITextView!
     @IBOutlet weak var secondTextField: UITextView!
     let networkManager = NetworkManager.shared
-    
+    var animationView: LottieAnimationView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        firstTextField.clipsToBounds = true
-        firstTextField.layer.cornerRadius = 10.0
-        
-        secondTextField.clipsToBounds = true
-        secondTextField.layer.cornerRadius = 10.0
-        
+        setUpTextField()
+       
         // Do any additional setup after loading the view.
     }
     
     
+    
+    func setUpTextField(){
+        firstTextField.clipsToBounds = true
+        firstTextField.layer.cornerRadius = 10.0
+        secondTextField.clipsToBounds = true
+        secondTextField.layer.cornerRadius = 10.0
+        secondTextField.isUserInteractionEnabled = false
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+            view.addGestureRecognizer(tapGesture)
+        
+    }
+    
+    @objc func dismissKeyboard() {
+        firstTextField.resignFirstResponder()
+    }
+    
     @IBAction func translateButton(_ sender: Any) {
         
         guard let inputText = firstTextField.text else {
-               print("Input text is empty")
-               return
-           }
-           
-           networkManager.getResponse(input: inputText) { result in
-               switch result {
-               case .success(let outputText):
-                   DispatchQueue.main.async {
-                       self.secondTextField.text = outputText
-                   }
-               case .failure(let error):
-                   print("Error translating text: \(error)")
-               }
-           }
+            print("Input text is empty")
+            return
+        }
+        
+        let prompt = "translate from English to Nigerian Pidgin:\n\n\(inputText)\n\nOutput:"
+        // Add animation view
+                animationView = LottieAnimationView(name: "loading")
+                animationView?.frame = view.bounds
+                animationView?.contentMode = .scaleAspectFit
+                view.addSubview(animationView!)
+                animationView?.play()
+        
+        networkManager.getTranslation(input: prompt) { result in
+            DispatchQueue.main.async {
+                            self.animationView?.stop()
+                            self.animationView?.removeFromSuperview()
+                        }
+            
+            switch result {
+            case .success(let outputText):
+                DispatchQueue.main.async {
+                    self.secondTextField.text = outputText
+                }
+            case .failure(let error):
+                print("Error translating text: \(error)")
+            }
+        }
         
     }
 }
-
